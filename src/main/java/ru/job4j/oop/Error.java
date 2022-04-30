a2499e285ef31e5df318a67e9ac4ec6400c40d6f
antonglavatskiy <glavazky@yandex.ru>
2022-04-30 20:54:36 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printError() {
+        System.out.println("Error is active: " + active);
+        System.out.println("status: " + status);
+        System.out.println("message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 404, "Not Found");
+        Error error3 = new Error(true, 400, "Bad Request");
+        error1.printError();
+        error2.printError();
+        error3.printError();
+    }
+
+}
