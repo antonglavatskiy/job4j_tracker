2318aa61239a2747e08793b11a5de3158ed9229c
antonglavatskiy <glavazky@yandex.ru>
2022-05-20 09:57:34 +0500

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ItemDescByNameTest {
+    @Test
+    public void descendingByName() {
+        Item item1 = new Item(1, "Hello");
+        Item item3 = new Item(3, "Simple");
+        Item item10 = new Item(10, "Current");
+        Item item5 = new Item(5, "Temporary");
+        List<Item> items = new ArrayList<>();
+        items.add(item1);
+        items.add(item3);
+        items.add(item10);
+        items.add(item5);
+        Collections.sort(items, new ItemDescByName());
+        List<Item> expected = new ArrayList<>();
+        expected.add(item5);
+        expected.add(item3);
+        expected.add(item1);
+        expected.add(item10);
+        assertThat(items, is(expected));
+    }
+}
