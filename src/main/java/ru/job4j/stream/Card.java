6be9d8aa289d442b1304be4254eb074504447222
antonglavatskiy <glavazky@yandex.ru>
2022-06-23 12:07:31 +0500

    5. Генерирование колоды карт. FlatMap [#504873]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -5,7 +5,9 @@ import java.util.stream.Stream;
 public class Card {
     private Suit suit;
     private Value value;
+    /*
 
+     */
     public Card(Suit suit, Value value) {
         this.suit = suit;
         this.value = value;
