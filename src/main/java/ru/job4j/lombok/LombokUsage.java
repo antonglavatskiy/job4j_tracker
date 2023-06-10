3c7f33e8e92b5a352d36f4dae920d5c9aa615b26
antonglavatskiy <glavazky@yandex.ru>
2023-06-10 08:47:10 +0500

    3. @Builder [#504890]

--- src/main/java/ru/job4j/lombok/LombokUsage.java
+++ src/main/java/ru/job4j/lombok/LombokUsage.java
@@ -6,5 +6,13 @@ public class LombokUsage {
         bird.setColor("white");
         bird.setAge(2);
         System.out.println(bird);
+
+        Role role = Role.of()
+                .id(1)
+                .name("user")
+                .accessBy("read")
+                .accessBy("update")
+                .build();
+        System.out.println(role);
     }
 }
