4642961676b23a0122e145788b2c5fe8429af7e6
antonglavatskiy <glavazky@yandex.ru>
2023-06-11 14:11:06 +0500

    4. Особенности использования Lombok [#504891]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,44 +1,38 @@
 package ru.job4j.tracker;
 
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
 public class Item {
+
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private int id;
+
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    @NonNull
     private String name;
+
     private LocalDateTime created = LocalDateTime.now();
+
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
-    public Item() {
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(int id, String name) {
+    public Item(int id, @NonNull String name) {
         this.id = id;
         this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id && Objects.equals(name, item.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
+    @ToString.Include
+    public String created() {
+        return FORMATTER.format(created);
     }
 }
