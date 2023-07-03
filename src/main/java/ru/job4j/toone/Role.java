180740cdc2ce0ff15c362d52bbff18fd1a883a9b
antonglavatskiy <glavazky@yandex.ru>
2023-07-03 10:02:26 +0500

    0. ToOne [#6873]

---
+++ src/main/java/ru/job4j/toone/Role.java
@@ -0,0 +1,18 @@
+package ru.job4j.toone;
+
+import javax.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "j_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+
+    private String name;
+}
