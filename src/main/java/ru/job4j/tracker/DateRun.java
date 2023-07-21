a9628c621285eabcea19cdb0caa8f0086f832556
antonglavatskiy <glavazky@yandex.ru>
2023-07-21 14:16:58 +0500

    5. Date [#331992]

---
+++ src/main/java/ru/job4j/tracker/DateRun.java
@@ -0,0 +1,36 @@
+package ru.job4j.tracker;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
+public class DateRun {
+    public static void main(String[] args) {
+        var registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try (var sf = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory()) {
+            var session = sf.openSession();
+            session.beginTransaction();
+            var stored = session.createQuery(
+                    "from Item", Item.class
+            ).list();
+            for (Item item : stored) {
+                var time = item.getCreated().atZone(
+                        TimeZone.getDefault().toZoneId()
+                ).format(DateTimeFormatter.ofPattern("HH:mm yyyy-MM-dd O"));
+                System.out.println(time);
+            }
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
